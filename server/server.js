ff47af28334504c53b7428ca1fb8b3f2d1eab022
Bamidele Azeem <lilmaya490@gmail.com>
2026-06-14 04:29:38 -0400

    fix: add deployed frontend URL to CORS allowed origins

--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://velario.vercel.app/',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
